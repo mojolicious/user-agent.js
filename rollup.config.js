1de8e871fa864163a9de55555adc1678db4b4b74
Sebastian Riedel <s@kraih.com>
2022-10-15 02:11:54 +0200

    Rely on rollup 3 defaults

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,7 @@ export default {
   input: Path.currentFile().sibling('lib', 'browser.js').toString(),
   output: {
     entryFileNames: '[name].js',
-    dir: Path.currentFile().sibling('dist').toString(),
-    format: 'es',
-    exports: 'named'
+    dir: Path.currentFile().sibling('dist').toString()
   },
   plugins: [resolve()]
 };

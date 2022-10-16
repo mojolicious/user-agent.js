428250bc5881ef07bd2d53ad7ddf01f2c29579f2
Sebastian Riedel <s@kraih.com>
2022-10-16 19:58:06 +0200

    Use less code for fetch transport

--- src/transport/http/fetch.ts
+++ src/transport/http/fetch.ts
@@ -2,13 +2,7 @@ import type {UserAgentRequestOptions} from '../../types.js';
 import {UserAgentResponse} from '../../response.js';
 
 export class FetchTransport {
-  async request(options: UserAgentRequestOptions): Promise<UserAgentResponse> {
-    return UserAgentResponse.fromWeb(
-      await fetch(options.url ?? '', {
-        body: options.body,
-        headers: options.headers,
-        method: options.method
-      })
-    );
+  async request({body, headers, method, url}: UserAgentRequestOptions): Promise<UserAgentResponse> {
+    return UserAgentResponse.fromWeb(await fetch(url ?? '', {body, headers, method}));
   }
 }
